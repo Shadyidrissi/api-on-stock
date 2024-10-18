6b41eafb8927286360ae00cab1c6d1cb4e056820
Shady Idrissi <chadiidac@gmail.com>
2024-10-18 14:17:59 +0100

    last

--- database/conecction.js
+++ database/conecction.js
@@ -1,16 +1,22 @@
-const mongoose =require("mongoose")
-const connectionDB = async () => {
-  mongoose
-    .connect("mongodb+srv://chadiidac:Ux2Uigkd3GW2U1PW@on-stock.w6j72.mongodb.net/?retryWrites=true&w=majority&appName=on-stock")
-    .catch((error) => handleError(error));
-  try {
-    await mongoose.connect("mongodb+srv://chadiidac:Ux2Uigkd3GW2U1PW@on-stock.w6j72.mongodb.net/?retryWrites=true&w=majority&appName=on-stock");
-    console.log("database is running");
-  } catch (error) {
-    handleError(error);
-  }
+const mongoose = require("mongoose");
+
+// دالة لمعالجة الأخطاء
+const handleError = (error) => {
+    console.error("Database connection error:", error);
 };
-module.exports=connectionDB 
+
+const connectionDB = async () => {
+    try {
+        // محاولة الاتصال بقاعدة البيانات
+        await mongoose.connect("mongodb+srv://chadiidac:Ux2Uigkd3GW2U1PW@on-stock.w6j72.mongodb.net/?retryWrites=true&w=majority&appName=on-stock");
+        console.log("Database is running");
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+module.exports = connectionDB;
+
 
 // Ux2Uigkd3GW2U1PW
 // chadiidac
